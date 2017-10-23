8d66e8fc45c9d24c9db74c5ed0cc73311c286b61
nickwu241 <nickwu241@gmail.com>
2017-10-23 09:49:39 -0700

    Submission for dubhacks 2017

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flask import Flask, jsonify
+
+import random
 import marvel
+
+spider_man_events = None
 
 app = Flask(__name__)
 @app.route('/')
@@ -8,13 +12,43 @@ def home():
 
 @app.route('/exampleEvent')
 def exampleEvent():
-    event = marvel.call_events()[0]
-    status, description, photo_url = marvel.parse_desc_and_photo(event)
-    response = {'status': status, 'description': description, 'photo_url': photo_url}
+    event = marvel.events()['results'][0]
+    response = marvel.wrap_parsed(marvel.parse_desc_and_photo(event))
     return jsonify(response)
+
+@app.route('/event/random')
+def randomEvent():
+    if not spider_man_events:
+        feteh_globals()
+    response = marvel.wrap_parsed(marvel.parse_desc_and_photo(random.choice(spider_man_events)))
+    return jsonify(response)
+
+@app.route('/event/<int:event_id>')
+def showEvent(event_id):
+    if not spider_man_events:
+        feteh_globals()
+    response = marvel.wrap_parsed(marvel.parse_desc_and_photo(spider_man_events[0]))
+    return jsonify(response)
+
+@app.route('/event/<int:event_id>/html')
+def showEventHtml(event_id):
+    if not spider_man_events:
+        feteh_globals()
+    status, description, photo_url = marvel.parse_desc_and_photo(spider_man_events[event_id])
+    return '<h1>{:s}</h1><img src={:s}>'.format(description, photo_url)
+
+def fetch_globals():
+    global spider_man_events
+    params = marvel.get_auth_params()
+    params['characters'] = marvel.CHAR_ID_MAP['Spider-Man']
+    params['offset'] = 0
+    params['limit'] = 100
+    spider_man_events = marvel.events(params)['results']
+    # print(spider_man_events)
 
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
+    fetch_globals()
     app.run(host='0.0.0.0', port=8080, debug=True)
 
--- marvel.py
+++ marvel.py
@@ -15,8 +15,11 @@ if not PUB_KEY or not PRIV_KEY:
     if not PUB_KEY or not PRIV_KEY:
         raise Exception("Make sure MARVEL_PUB_KEY and MARVEL_PRIV_KEY is present.")
 
+CHAR_ID_MAP = {'Spider-Man': 1009610}
+
 BASE_API = "http://gateway.marvel.com"
 EVENTS = "/v1/public/events"
+STORIES = "/v1/public/stories"
 
 def get_ts_and_hash():
     ts = str(int(time.time()))
@@ -27,20 +30,45 @@ def get_auth_params():
     ts, hash = get_ts_and_hash()
     return {'ts': ts, 'apikey': PUB_KEY, 'hash': hash}
 
-def call_events():
-    response = requests.get(BASE_API + EVENTS, params=get_auth_params()).json()
+def events(params={}):
+    params = {**params, **get_auth_params()}
+    response = requests.get(BASE_API + EVENTS, params=params).json()
     assert response['code'] == 200
-    return response['data']['results']
+    return response['data']
 
 def parse_desc_and_photo(event):
     desc = event['description']
-    image_url = event['thumbnail']['path'] + '.' + event['thumbnail']['extension']
+    if 'thumbnail' in event:
+        image_url = event['thumbnail']['path'] + '.' + event['thumbnail']['extension']
+    else:
+        image_url = None
     status = 200
-    return (200, desc, image_url)
+    return (status, desc, image_url)
+
+def wrap_parsed(parsed_tuple):
+    status, description, photo_url = parsed_tuple
+    return {'status': status, 'description': description, 'photo_url': photo_url}
 
 def main():
-    event = call_events()[0]
+    event = call_events()['results'][0]
     print(parse_desc_and_photo(event))
 
 if __name__ == '__main__':
-    main()
+    #main()
+    import pprint
+    response = events({'characters': CHAR_ID_MAP['Spider-Man']}) 
+    offset = 0
+    total = response['total']
+    while offset < total:
+        print(offset)
+        params = get_auth_params()
+        params['characters'] = CHAR_ID_MAP['Spider-Man']
+        params['offset'] = offset
+        params['limit'] = 100
+        response = events(params) 
+        for res in response['results']:
+            print(parse_desc_and_photo(res))
+        offset += 100
+        break
+    # pprint.pprint(response)
+
